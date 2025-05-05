402ddea628ccda3fc587bff5e6fcdff690c4e7cb
福福 <evisu@fufudeMacBook-Pro.local>
2025-05-05 17:17:26 +0800

    test

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ await connnectCloudinary()
 
 
 // // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173','https://vercel.com/evisus-projects/front-backend-two']
 
 // Middleware configuration
 app.use(express.json())
